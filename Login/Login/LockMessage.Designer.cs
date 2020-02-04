663cee2a28154c1efaf50a9a0d1bac6f51661f7b
Tim <tstamper@ksu.edu>
2020-02-03 20:42:58 -0600

    Added meat to the logic

--- Login/Login/LockMessage.Designer.cs
+++ Login/Login/LockMessage.Designer.cs
@@ -38,10 +38,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(349, 34);
+            this.label1.Size = new System.Drawing.Size(349, 17);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Your account has been locked after 3 failed attempts. \r\nPleas try again in: 3 Hou" +
-    "rs. \r\n";
+            this.label1.Text = "Your account has been locked after 3 failed attempts. \r\n";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
--- Login/Login/loginScreen.cs
+++ Login/Login/loginScreen.cs
@@ -51,17 +51,23 @@ namespace Login
             DataTable dt = new DataTable();
             da.Fill(dt);
 
+            int attempt_per_session = Int32.Parse(dt.Rows[0][2].ToString());
+            int timer = Int32.Parse(dt.Rows[0][5].ToString());
+            int locked = Int32.Parse(dt.Rows[0][6].ToString());
+            int accesslevel = Int32.Parse(dt.Rows[0][7].ToString());
+            int failed_attempts = Int32.Parse(dt.Rows[0][3].ToString());
+            int successful_attempts = Int32.Parse(dt.Rows[0][4].ToString());
 
 
             //If username doesnt exist
             if (dt.Rows.Count < 1)
             {
-                int attempt_per_session = 1;
-                int timer = 0;
-                int locked = 0;
-                int accesslevel = 0;
-                int failed_attempts = 1;
-                int successful_attempts = 0;
+                attempt_per_session = 1;
+                timer = 0;
+                locked = 0;
+                accesslevel = 0;
+                failed_attempts = 1;
+                successful_attempts = 0;
                 SqlCommand addUser = new SqlCommand("INSERT INTO attempts (username, password, attempt_per_session, timer, lock, accesslevel, failed_attempts, successful_attempts) VALUES('" + userID + password + attempt_per_session + timer + locked + accesslevel + successful_attempts + failed_attempts + "')", conn);
             }
             else
@@ -70,6 +76,7 @@ namespace Login
                 if (dt.Rows[0][1].ToString() == null)
                 {
                     //Increment failed attempts
+                    failed_attempts++; 
 
                     //If permalocked
                     if (dt.Rows[0][6].ToString() == "1")
@@ -80,17 +87,18 @@ namespace Login
                     if (dt.Rows[0][3].ToString() == "3")
                     {
                         //Lock
-
+                        locked = 1; 
                     }
                     else if (dt.Rows[0][3].ToString() == "6")
                     {
                         //Permalock
-
+                        locked = 1;
                     }
                 }
                 else
                 {
                     //Increment fail counter
+                    failed_attempts++;
 
                     //If permalocked
                     if (dt.Rows[0][6].ToString() == "1")
@@ -114,34 +122,45 @@ namespace Login
                             {
                                 //Lock 3 hours
 
+
                             }
                             else if (dt.Rows[0][3].ToString() == "6")
                             {
                                 //Permalock
-
+                                locked = 1;
                             }
                             else
                             {
                                 //Display password incorrect message and amount of fails
+                                LockMessage Form2 = new LockMessage();
+                                Form2.ShowDialog();
+
                             }
                         }
                         //If password is right
                         else
                         {
                             //Increment successful attempts
+                            successful_attempts++;
 
 
                             //Reset failed attempts
+                            attempt_per_session = 0;
+                          
 
 
                             //If employee is a worker
                             if (dt.Rows[0][7].ToString() == "Worker")
                             {
                                 //Show worker screen
+                                workerView Form3 = new workerView();
+                                Form3.ShowDialog();
                             }
                             else
                             {
                                 //Show manager screen
+                                managerView Form4 = new managerView();
+                                Form4.ShowDialog();
                             }
                         }
                     }
