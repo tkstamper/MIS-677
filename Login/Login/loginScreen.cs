d127d1c3fc3d91ee6b99ec9e685fec0a6609db90
raylm <32377992+raylm@users.noreply.github.com>
2020-02-04 01:08:03 -0600

    Add files via upload
    Up to date version

--- Login/Login/loginScreen.cs
+++ Login/Login/loginScreen.cs
@@ -51,125 +51,136 @@ namespace Login
             DataTable dt = new DataTable();
             da.Fill(dt);
 
-            int attempt_per_session = Int32.Parse(dt.Rows[0][2].ToString());
+            int session_attempts = Int32.Parse(dt.Rows[0][2].ToString());
+            //convert timer to datetime
             int timer = Int32.Parse(dt.Rows[0][5].ToString());
             int locked = Int32.Parse(dt.Rows[0][6].ToString());
             int accesslevel = Int32.Parse(dt.Rows[0][7].ToString());
-            int failed_attempts = Int32.Parse(dt.Rows[0][3].ToString());
+            int fails = Int32.Parse(dt.Rows[0][3].ToString());
             int successful_attempts = Int32.Parse(dt.Rows[0][4].ToString());
 
 
             //If username doesnt exist
             if (dt.Rows.Count < 1)
             {
-                attempt_per_session = 1;
+                session_attempts = 1;
                 timer = 0;
                 locked = 0;
                 accesslevel = 0;
-                failed_attempts = 1;
+                fails = 1;
                 successful_attempts = 0;
-                SqlCommand addUser = new SqlCommand("INSERT INTO attempts (username, password, attempt_per_session, timer, lock, accesslevel, failed_attempts, successful_attempts) VALUES('" + userID + password + attempt_per_session + timer + locked + accesslevel + successful_attempts + failed_attempts + "')", conn);
+                SqlCommand addUser = new SqlCommand("INSERT INTO attempts (username, password, attempt_per_session, timer, lock, accesslevel, failed_attempts, successful_attempts) VALUES('" + userID + password + session_attempts + timer + locked + accesslevel + successful_attempts + fails + "')", conn);                
             }
             else
             {
                 //If no password is found (account doesnt exist)
-                if (dt.Rows[0][1].ToString() == null)
+                if (dt.Rows[0][1].ToString() == "")
                 {
-                    //Increment failed attempts
-                    failed_attempts++; 
-
                     //If permalocked
-                    if (dt.Rows[0][6].ToString() == "1")
+                    if (Int32.Parse(dt.Rows[0][6].ToString()) == 1)
                     {
-                        //Display permalock
-                    }
-                    //If failed attempts = 3
-                    if (dt.Rows[0][3].ToString() == "3")
-                    {
-                        //Lock
-                        locked = 1; 
-                    }
-                    else if (dt.Rows[0][3].ToString() == "6")
-                    {
-                        //Permalock
-                        locked = 1;
-                    }
-                }
-                else
-                {
-                    //Increment fail counter
-                    failed_attempts++;
-
-                    //If permalocked
-                    if (dt.Rows[0][6].ToString() == "1")
-                    {
-                        //Display timer
-
+                        MessageBox.Show("Your account is permanently locked. Please contact an administrator to reset your password.");
                     }
                     //If timer isnt 0
                     else if (dt.Rows[0][5].ToString() != "0")
                     {
                         //Display timer
+                        MessageBox.Show("Your account is temporarily locked for " + timer + " more seconds. Please try again later.");
+                    }
+                    else
+                    {
+                        //Increment failed attempts
+                        fails++;
 
+                        //Increment session fails
+                        session_attempts++;
+
+                        //Update new fails and session fails in the table
+                        SqlCommand updateFails = new SqlCommand("UPDATE attempts (failed_attempts, attempt_per_session) VALUES('" + fails + session_attempts + "') WHERE username = '" + userID + "'", conn);
+                        updateFails.ExecuteNonQuery();
+                        MessageBox.Show(dt.Rows[0][3].ToString());
+                        //If failed attempts = 3
+                        if (dt.Rows[0][3].ToString() == "3")
+                        {
+                            //Lock
+                            MessageBox.Show("Your account has been temporarily locked for " + timer + " seconds. Please try again later.");
+                        }
+                        else if (dt.Rows[0][3].ToString() == "6")
+                        {
+                            //Permalock
+                            locked = 1;
+                            MessageBox.Show("Your account has been permanently locked. Please contact an administrator to reset your password.");
+                        }
+                    }
+                }
+                //Password is found (account exists)
+                else
+                {
+                    //If permalocked
+                    if (Int32.Parse(dt.Rows[0][6].ToString()) == 1)
+                    {
+                        //Display permalock
+                        MessageBox.Show("Your account is permanently locked. Please contact an administrator to reset your password.");
+                    }
+                    //If timer isnt 0
+                    else if (dt.Rows[0][5].ToString() != "0")
+                    {
+                        //Display timer
+                        MessageBox.Show("Your account is temporarily locked for " + timer + " more seconds. Please try again later.");
                     }
                     else
                     {
                         //If password is wrong
                         if (dt.Rows[0][1].ToString() != password)
                         {
+                            //Increment failed attempts
+                            fails++;
+                            //Increment session fails
+                            session_attempts++;
+
                             //If failed attempts = 3
                             if (dt.Rows[0][3].ToString() == "3")
                             {
                                 //Lock 3 hours
-
-
+                                MessageBox.Show("Your account has been locked for " + timer + " seconds. Please try again later.");
                             }
                             else if (dt.Rows[0][3].ToString() == "6")
                             {
                                 //Permalock
-                                locked = 1;
+                                MessageBox.Show("Your account has been permanently locked. Please contact an administrator to reset your password.");
                             }
                             else
                             {
                                 //Display password incorrect message and amount of fails
-                                LockMessage Form2 = new LockMessage();
-                                Form2.ShowDialog();
-
                             }
                         }
                         //If password is right
                         else
                         {
                             //Increment successful attempts
-                            successful_attempts++;
 
 
                             //Reset failed attempts
-                            attempt_per_session = 0;
-                          
 
 
                             //If employee is a worker
                             if (dt.Rows[0][7].ToString() == "Worker")
                             {
                                 //Show worker screen
-                                workerView Form3 = new workerView();
-                                Form3.ShowDialog();
                             }
                             else
                             {
                                 //Show manager screen
-                                managerView Form4 = new managerView();
-                                Form4.ShowDialog();
                             }
                         }
                     }
                 }
-                MessageBox.Show(dt.Rows[0][0].ToString());
-
             }
         }
 
-      
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
